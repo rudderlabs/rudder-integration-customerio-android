785a6414ea7e1b7f067d33f03b1c2a1ae2b3ab01
Arnab Pal <arnab.compsc@gmail.com>
2024-10-07 23:27:24 +0530

    feat: upgraded customer.io SDK version to 4.2.0

--- sample-kotlin/src/main/java/com/rudderlabs/android/sample/kotlin/MainApplication.kt
+++ sample-kotlin/src/main/java/com/rudderlabs/android/sample/kotlin/MainApplication.kt
@@ -1,11 +1,15 @@
 package com.rudderlabs.android.sample.kotlin
 
 import android.app.Application
-import com.rudderlabs.android.sample.kotlin.BuildConfig
 import com.rudderstack.android.integration.customerio.CustomerIOIntegrationFactory
 import com.rudderstack.android.sdk.core.RudderClient
 import com.rudderstack.android.sdk.core.RudderConfig
 import com.rudderstack.android.sdk.core.RudderLogger
+import io.customer.messaginginapp.MessagingInAppModuleConfig
+import io.customer.messaginginapp.ModuleMessagingInApp
+import io.customer.messagingpush.ModuleMessagingPushFCM
+import io.customer.sdk.CustomerIOBuilder
+import io.customer.sdk.data.model.Region
 
 class MainApplication : Application() {
     companion object {
@@ -26,5 +30,24 @@ class MainApplication : Application() {
                 .withSleepCount(3)
                 .build()
         )
+
+        // important to add right after initialization
+        rudderClient.onIntegrationReady("Customer IO") {
+            val siteId = (it as CustomerIOIntegrationFactory).siteId
+            val builder: CustomerIOBuilder = it.builder
+            val region: Region = it.region
+
+            builder.addCustomerIOModule(ModuleMessagingPushFCM())
+            builder.addCustomerIOModule(
+                ModuleMessagingInApp(
+                    config = MessagingInAppModuleConfig.Builder(
+                        siteId,
+                        region
+                    ).build()
+                )
+            )
+
+            builder.build()
+        }
     }
 }
